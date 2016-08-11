006415c7d32d61418d4b5739361ce058b8731e8a
b-ellis <bradjellis18@gmail.com>
2016-08-11 09:55:28 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var getEvents = function(tags) {
+	
+	var params = {
+		key: 
+	};
+
+	$.ajax({
+		url: ,
+		data: params
+	});
+
+	console.log(this);
+}
+
+$(function(){
+	var searchTerm = $('#query').val();
+	var tags = searchTerm
+	getEvents(tags);
+});
