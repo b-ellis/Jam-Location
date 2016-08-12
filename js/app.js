0fcadf5f399af7296fcb35864a91e89b7b59e2a8
b-ellis <bradjellis18@gmail.com>
2016-08-12 07:40:48 -0700

    content added

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
-var getEvents = function(tags) {
+var getEvents = function(location) {
 	
-	var params = {
-		key: 
+	var myData = {
+		zipCode: location,
+		radius: 50,
+		page: 0,
+		api_key: "vrchjvtc2yyx7wzs56hsuprd",
+		o: "json"
 	};
 
 	$.ajax({
-		url: ,
-		data: params
+		url: "http://api.jambase.com/events?",
+		data: myData,
+		type: "GET"
 	});
 
-	console.log(this);
+	console.log(myData);
 }
 
 $(function(){
-	var searchTerm = $('#query').val();
-	var tags = searchTerm
-	getEvents(tags);
+	$(".search").submit(function(event){
+		event.preventDefault();
+		var location = $(this).find("input[name='location']").val();
+		getEvents(location);
+	});
 });
