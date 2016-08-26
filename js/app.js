7d66049a01c13cdb0afbff6e5f130afc7aa3fbb8
b-ellis <bradjellis18@gmail.com>
2016-08-25 17:11:47 -0700

    checking geolocation

--- js/app.js
+++ js/app.js
@@ -89,15 +89,19 @@ var getEvents = function(location) {
 
 $(function(){
 	initMap();
-	navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-       	lat: position.coords.latitude,
-       	lng: position.coords.longitude
-  	};
+	function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    	}
+    	console.log(this);
+	}
 
-  	console.log(pos);
-
-  	});
+	function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+	}
 
   	console.log(position);
 	$("#search").submit(function(event){
