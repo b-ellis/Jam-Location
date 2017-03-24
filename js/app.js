57a63776c841c8fcd9c891a1ca3267d319240547
b-ellis <bradjellis18@gmail.com>
2017-03-24 13:46:04 -0700

    bugs fixed

--- js/app.js
+++ js/app.js
@@ -3,77 +3,34 @@ var marker;
 var infoWindow;
 
 
-function initMap() {
+var initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.730610, lng: -73.935242},
        	zoom: 8
     });
     var input = document.getElementById('search');
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    getLocation();
 };
 
-function getLocation(){
-	var infoWindow = new google.maps.InfoWindow({map: map});
-    if (navigator.geolocation) {
-    	navigator.geolocation.getCurrentPosition(function(position) {
-      	var pos = {
-        	lat: position.coords.latitude,
-        	lng: position.coords.longitude
-      	};
-
-      	console.log(pos);
-
-    	infoWindow.setPosition(pos);
-    	infoWindow.setContent('Location found. Click "Search for Concerts" to find concerts near you.');
-    	map.setCenter(pos);
-    	}, function() {
-      		handleLocationError(true, infoWindow, map.getCenter());
-    	});
-  	} else {
-    	// Browser doesn't support Geolocation
-    	handleLocationError(false, infoWindow, map.getCenter());
-  	}	
-
-  	var yourZip = function (pos) {
-		geocoder.geocode({ 
-	  		'latLng': pos, 
-	  		'region': 'US' 
-	  	}, function (results, status) {
-	    	response($.map(results, function (item) {
-	      		return (item.address_components.postal_code);//This is what you want to look at
-	      		console.log(item.address_components.postal_code);
-	      	}));
-	    });
-	}
+function getInfoCallback(map, content) {
+    var infowindow = new google.maps.InfoWindow({
+    	content: content
+    });
+    return function() {
+        infowindow.setContent(content); 
+        infowindow.open(map, this);
+    };
 }
 
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
-
-function setMarker(latlong){
+function setMarker(latlong, artist, venue, venueUrl, address){
 	marker = new google.maps.Marker({
 		position: new google.maps.Marker(latlong),
 		animation: google.maps.Animation.DROP,
 		map: map
 	});
-	displayInfo();
+	var contentString = '<div>'+ artist + '<br /><a href='+venueUrl +'>'+ venue +'</a><br />' + address + '<br />Concert Info Provided By <a href="http://www.JamBase.com" target="_top" title="JamBase Concert Search">JamBase<span style="font-size: 12px; white-space: normal;" _mce_style="font-size: 12px; white-space: normal;"> </span></a></div>';
+	google.maps.event.addListener(marker, 'click', getInfoCallback(map, contentString));
 };
-
-var displayInfo = function(){
-	infoWindow = new google.maps.InfoWindow({
-		content: "event info"
-	});
-	marker.addListener(marker, 'click', function(marker, infoWindow) {
-    	infoWindow.open(marker, map);
-    	windows.push(infoWindow);
-  	});
-}
 
 var getEvents = function(location) {
 
@@ -93,14 +50,32 @@ var getEvents = function(location) {
 
 	.done(function(result){
 		$.each(result.Events, function(index, event){
-			var artist = event.Artists.Name;
+			var artist = event.Artists[0].Name;
 			var venue = event.Venue.Name;
 			var venueUrl = event.Venue.Url;
 			var address = event.Venue.Address + ", " + event.Venue.City + ", " + event.Venue.State + ", " + event.Venue.CountryCode + ", " + event.Venue.ZipCode;
 			var latlong = {lat: event.Venue.Latitude, lng: event.Venue.Longitude};
-			setMarker(latlong);
+			setMarker(latlong, artist, venue, venueUrl, address);
 		});
 	});
+}
+
+function relocate(location){
+	var lat = '';
+    var lng = '';
+    var address = location;
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			lat = results[0].geometry.location.lat();
+			lng = results[0].geometry.location.lng();
+			var latlng = new google.maps.LatLng(lat, lng);
+			map.setZoom(9);		
+			map.panTo(latlng);
+	    } else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+    });
 }
 
 $(function(){
@@ -108,5 +83,6 @@ $(function(){
 		event.preventDefault();
 		var location = $(this).find("input[name='location']").val();
 		getEvents(location);
+		relocate(location)
 	});
 });
